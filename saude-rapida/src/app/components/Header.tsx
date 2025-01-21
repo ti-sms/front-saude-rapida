d33df364452b31e11949cbfaf344174a14bee439
neemias-araujo <neemiassms@gmail.com>
2025-01-21 15:59:03 -0300

    initial commit

---
+++ saude-rapida/src/app/components/Header.tsx
@@ -0,0 +1,37 @@
+"use client"
+import Image from "next/image";
+import { Button } from "primereact/button";
+import { Dropdown } from "primereact/dropdown";
+import { useState } from "react";
+
+export default function Header() {
+    const [user, setUser] = useState({
+        name: "Neemias Araújo",
+        userPermissions: ["admin"],
+    });
+
+    return (
+        <>
+            <div className="w-screen h-24 bg-amber-500 flex items-center">
+                <div className="mt-10 ml-2 p-3">
+                    <Image
+                        aria-hidden
+                        src="/logo_branca.png"
+                        alt="Saúde Rápida"
+                        width={180}
+                        height={120}
+                        className="mb-10" 
+                    />
+                </div>
+                <div className="flex justify-end w-full m-3 space-x-2 p-3">
+                    <div className="text-white flex items-center">
+                        <p className="">{user.name}</p>
+                    </div>
+                    <Dropdown options={user.userPermissions} value={user.userPermissions[0]} className="w-50"/>
+                    <Button icon="pi pi-cog" className="" />
+                    <Button icon="pi pi-sign-out" severity="danger" />
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ saude-rapida/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "primereact/button";
+
+export default function Home() {
+  return (
+   <>
+  
+   </>
+  );
+}
