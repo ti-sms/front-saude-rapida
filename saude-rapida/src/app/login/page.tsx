e3060d56838888dbc34e3ef6b3f947da6fb06d53
neemias-araujo <neemiassms@gmail.com>
2025-01-25 14:54:42 -0300

    [RF001][SRD-75] login page created

--- saude-rapida/src/app/login/page.tsx
+++ saude-rapida/src/app/login/page.tsx
@@ -1,8 +1,29 @@
-export default function Login(){
-    return(
+import Image from "next/image";
+
+export default function Login() {
+    return (
         <div className="w-full h-screen flex justify-center items-center">
-            <div className="bg-white w-[40rem] h-[40rem] shadow-2xl rounded-lg flex items-center justify-center">
-               
+            <div className="bg-white w-[40rem] h-[40rem] shadow-2xl rounded-3xl flex items-center justify-center">
+                <div className="w-1/2 bg-black h-full rounded-l-3xl">
+
+                    <Image
+                        aria-hidden
+                        src="/images/login.png"
+                        alt="Saúde Rápida"
+                        width={700}
+                        height={700}
+                        className="w-full h-full object-cover rounded-l-lg"
+                    />
+
+                </div>
+                <div className="w-1/2 p-2">
+                    <h1 className="text-3xl font-bold mb-2 p-2">Login</h1>
+                    <form className="flex flex-col space-y-4">
+                        <input type="text" placeholder="Usuário" className="border border-gray-300 p-2 rounded-lg" />
+                        <input type="password" placeholder="Senha" className="border border-gray-300 p-2 rounded-lg" />
+                        <button type="submit" className="bg-blue-500 text-white p-2 rounded-lg">Entrar</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
